e03d1456056a93fd672eea268fddbd41fd86281c
unknown <ntnicholasthie@gmail.com>
2019-05-17 16:44:11 +0700

    Added showing voting contract status and result

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,50 @@
 import React, { Component } from "react";
+import { Header } from 'semantic-ui-react';
+
+function ShowVotingContractInfo(props) {
+  return (
+    <div>
+      <ShowVotingContractStatus status={props.votingContract.status} />
+      { props.votingContract.status === "Finished" && <ShowVotingResult result={props.votingContract.result} /> }
+    </div>
+  );
+}
+
+function ShowVotingContractStatus(props) {
+  return (
+    <div>
+      <Header as='h1'>
+        Voting contract status : {props.status}
+      </Header>
+    </div>
+  )
+}
+
+function ShowVotingResult(props) {
+  return (
+    <div>
+      <Header as='h2'>
+        Voting result : {props.result}
+      </Header>
+    </div>
+  )
+}
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votingContract: {
+        status: "Preparation",
+        result: "Voting result"
+      }
+    }
+  }
+
   render() {
     return (
       <div>
-        Home
+        <ShowVotingContractInfo votingContract={this.state.votingContract} />
       </div>
     );
   }
