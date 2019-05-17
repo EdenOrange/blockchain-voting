09a8583162683c7449410adca9411b8f018b3c13
unknown <ntnicholasthie@gmail.com>
2019-05-17 19:15:30 +0700

    Added account info list

--- src/PreparationVoter.js
+++ src/PreparationVoter.js
@@ -1,10 +1,69 @@
 import React, { Component } from "react";
+import { Icon, List } from 'semantic-ui-react';
+
+function ShowAccountInfoList(props) {
+  const {accounts} = props;
+  const Accounts = accounts.map((account) => ShowAccountInfo(account));
+
+  return (
+    <List divided>
+      {Accounts}
+    </List>
+  );
+}
+
+function ShowAccountInfo(account) {
+  return (
+    <List.Item key={account.address}>
+      {account.address}
+      <List.List>
+        <List.Item>
+          Registered : {account.registered ? <Icon name='check' /> : <Icon name='close' />}
+        </List.Item>
+        {account.registered && 
+        <List.Item>
+          Blinded Vote : {account.blindedVote}
+        </List.Item>
+        }
+        {account.registered && 
+        <List.Item>
+          Signed Blinded Vote : {account.signedBlindedVote}
+        </List.Item>
+        }
+      </List.List>
+    </List.Item>
+  );
+}
 
 class PreparationVoter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: [
+        {
+          address: '0xAddress001',
+          registered: true,
+          blindedVote: 'blindedVote001',
+          signedBlindedVote: 'signedBlindedVote001'
+        },
+        {
+          address: '0xAddress002',
+          registered: false,
+        },
+        {
+          address: '0xAddress003',
+          registered: true,
+          blindedVote: 'blindedVote003',
+          signedBlindedVote: 'signedBlindedVote003'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-        Preparation Voter
+        <ShowAccountInfoList accounts={this.state.accounts} />
       </div>
     );
   }
