0dcbf1ef18ff89827dffdd72da73fe66d63c8eb3
unknown <ntnicholasthie@gmail.com>
2019-05-17 17:57:23 +0700

    Added Add New Organizer

--- src/PreparationOrganizer.js
+++ src/PreparationOrganizer.js
@@ -1,10 +1,41 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { Input } from 'semantic-ui-react';
+
+function AddOrganizer(props) {
+  const [value, setValue] = useState('');
+
+  return (
+    <div>
+      <Input
+        fluid
+        action={{
+          content: 'Add New Organizer',
+          onClick: () => props.onClick(value)
+        }}
+        placeholder='New Organizer public address...'
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <br />
+    </div>
+  );
+}
 
 class PreparationOrganizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  handleAddOrganizer(publicAddress) {
+    console.log("Add organizer : " + publicAddress);
+  }
+
   render() {
     return (
       <div>
-        Preparation Organizer
+        <AddOrganizer onClick={(publicAddress) => this.handleAddOrganizer(publicAddress)} />
       </div>
     );
   }
