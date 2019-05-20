0c04d744d74ca98dd1ea4fd232deadc6fa519ce3
unknown <ntnicholasthie@gmail.com>
2019-05-20 15:38:16 +0700

    Added get unblinded vote and cast vote

--- src/VoteCastingVoter.js
+++ src/VoteCastingVoter.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { Divider, Dropdown, Header } from 'semantic-ui-react';
+import React, { Component, useState } from "react";
+import { Button, Divider, Dropdown, Header, Input } from 'semantic-ui-react';
+import * as BlindSignature from 'blind-signatures';
 
 function BallotSignedInfo(props) {
   const {voters, voterAccount} = props;
@@ -45,7 +46,7 @@ function AccountsList(props) {
   return (
     <div>
       <Header>
-        Your Accounts
+        Select an account to cast vote from
       </Header>
       <Dropdown
         defaultValue={selectedAccount}
@@ -56,6 +57,30 @@ function AccountsList(props) {
       />
     </div>
   )
+}
+
+function CastVote(props) {
+  const {handleCastVote} = props;
+  const [voteString, setVoteString] = useState('');
+  const [randomValue, setRandomValue] = useState('');
+
+  return (
+    <div>
+      <Input
+        fluid
+        placeholder='Vote string...'
+        onChange={(e) => setVoteString(e.target.value)}
+      />
+      <Input
+        fluid
+        placeholder='Random value...'
+        onChange={(e) => setRandomValue(e.target.value)}
+      />
+      <Button primary onClick={() => handleCastVote(voteString, randomValue)}>
+        Cast Vote!
+      </Button>
+    </div>
+  );
 }
 
 class VoteCastingVoter extends Component {
@@ -101,6 +126,35 @@ class VoteCastingVoter extends Component {
             signed: '116462440',
             organizerSignerId: '1'
           }
+        ],
+        organizers: [ // Organizer account
+          {
+            id: '1',
+            address: '0xAddressOrg001',
+            name: 'Org1',
+            blindSigKey: { // RSA keypair
+              N: 29966692371364866625346898353663834134938385542002417037721577477302102136522085939472165345604159090008369291967229214089843550402783764345171829774118370421727069328975236719404868237298550523989366936116144150572205603225580613276301181810980227503747111091217434069794434110873713548193276565135873156551776781744977506384102252699464204349946745613824014413457618301726927010747822355674379832350188825717228418277968661184894099448068813151646552494933847934355517511397146924721973857101644904751900691439081133481472498369847582949341542277140564618444421223545987899994237990578140719418027385682765400810787,
+              E: 65537
+            }
+          },
+          {
+            id: '2',
+            address: '0xAddressOrg002',
+            name: 'Org2',
+            blindSigKey: {
+              N: 26458970144176529231278251478923876274581682945160630211923288556492285074551812817562954732957996890856225708775063710963505080257659492810452589951505161037289950403780584424848427034382647213927966088689259773969220432507351297342517879366775210437071365276681805376039406952355809450845271748916810754673242718443536347275958685398465694101786361797986578701119612608844428824062586235437039817380062355856918189315480847660878662520951404772466376423110443487482138333887388449385867806705579043254110057834686860727136742974754338482731437361733622421487674030607511342064989032215407906503194121394165185383797,
+              E: 65537
+            }
+          },
+          {
+            id: '3',
+            address: '0xAddressOrg003',
+            name: 'Org3',
+            blindSigKey: {
+              N: 22174933060533612279001839757293277558447636625990692079946257223347032594943597488661968989040186738935132503446965285010055923873177882577484769463195499109178479501165613614043668876610322147551149096608999387482124244500100367766728152174625193206582596173771525689770346002747421347904671941536291573608005763407437768868993893358554277826661660885093146948296152543868273304544471129280791068975551329074059044323118982282750362281455605896422650475741100638391510715626109674650392999767613861555822958045214362249243277613528444450124436766949894713659037566339120415451310965138637574175225632109970533494113,
+              E: 65537
+            }
+          }
         ]
       },
       accounts: [
@@ -141,12 +195,13 @@ class VoteCastingVoter extends Component {
           address: '0xAddress106'
         },
       ],
-      selectedAccount: 0
+      voterAccount: 0, // Selected wallet account
+      selectedAccount: 0 // Another account to be cast vote anonymously
     }
   }
 
   getCurrentVoterAccount = () => {
-    return this.state.accounts[this.state.selectedAccount];
+    return this.state.accounts[this.state.voterAccount];
   }
 
   handleSelectedAccount = (selectedAccount) => {
@@ -155,12 +210,36 @@ class VoteCastingVoter extends Component {
     })
   }
 
+  getUnblindedVote = (voter, randomValue) => {
+    const organizerSigner = this.state.votingContract.organizers.find(organizer => organizer.id === voter.organizerSignerId);
+    const unblinded = BlindSignature.unblind({
+      signed: voter.signed,
+      N: organizerSigner.blindSigKey.N,
+      r: randomValue
+    });
+    console.log("Unblinded vote : " + unblinded);
+    return unblinded;
+  }
+
+  handleCastVote = (voteString, randomValue) => {
+    const voter = this.state.votingContract.voters.find(voter => voter.address === this.state.accounts[this.state.voterAccount].address);
+    const unblinded = this.getUnblindedVote(voter, randomValue);
+
+    // Send cast vote to VotingContract
+    const message = {
+      voteString: voteString,
+      unblinded: unblinded.toString()
+    }
+    console.log("Send : " + JSON.stringify(message));
+  }
+
   render() {
     return (
       <div>
         <BallotSignedInfo voters={this.state.votingContract.voters} voterAccount={this.getCurrentVoterAccount()} />
         <Divider />
         <AccountsList accounts={this.state.accounts} selectedAccount={this.state.selectedAccount} handleSelectedAccount={this.handleSelectedAccount} />
+        <CastVote handleCastVote={this.handleCastVote} />
       </div>
     );
   }
