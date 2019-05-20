d3808057f7c30cf43213a413f5456adb14e235ec
unknown <ntnicholasthie@gmail.com>
2019-05-20 14:48:19 +0700

    Added show ballot signed info

--- src/VoteCastingVoter.js
+++ src/VoteCastingVoter.js
@@ -1,10 +1,134 @@
 import React, { Component } from "react";
+import { Header } from 'semantic-ui-react';
+
+function BallotSignedInfo(props) {
+  const {voters, voterAccount} = props;
+  const voter = voters.find(voter => voter.address === voterAccount.address);
+  
+  if (voter === undefined) {
+    return (
+      <div>
+        <Header>
+          This account is not registered in voting
+        </Header>
+      </div>
+    )
+  }
+  else if (voter.signed === '') {
+    return (
+      <div>
+        <Header>
+          Waiting for Organizer to sign your Ballot
+        </Header>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        <Header>
+          Organizer has signed your Ballot
+        </Header>
+      </div>
+    )
+  }
+}
 
 class VoteCastingVoter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votingContract: {
+        voters: [
+          {
+            address: '0xAddress001',
+            name: 'Name1',
+            blinded: '106380571',
+            signed: '50914715',
+            organizerSignerId: '3'
+          },
+          {
+            address: '0xAddress002',
+            blinded: '106380571',
+            signed: '140035364',
+            organizerSignerId: '2'
+          },
+          {
+            address: '0xAddress003',
+            blinded: '106380571',
+            signed: '44060348',
+            organizerSignerId: '3'
+          },
+          {
+            address: '0xAddress004',
+            blinded: '106380571',
+            signed: '87594437',
+            organizerSignerId: '2'
+          },
+          {
+            address: '0xAddress005',
+            blinded: '106380571',
+            signed: '188234335',
+            organizerSignerId: '1'
+          },
+          {
+            address: '0xAddress006',
+            blinded: '106380571',
+            signed: '116462440',
+            organizerSignerId: '1'
+          }
+        ]
+      },
+      accounts: [
+        {
+          address: '0xAddress001'
+        },
+        {
+          address: '0xAddress002'
+        },
+        {
+          address: '0xAddress003'
+        },
+        {
+          address: '0xAddress004'
+        },
+        {
+          address: '0xAddress005'
+        },
+        {
+          address: '0xAddress006'
+        },
+        {
+          address: '0xAddress101'
+        },
+        {
+          address: '0xAddress102'
+        },
+        {
+          address: '0xAddress103'
+        },
+        {
+          address: '0xAddress104'
+        },
+        {
+          address: '0xAddress105'
+        },
+        {
+          address: '0xAddress106'
+        },
+      ],
+      selectedAccount: 0
+    }
+  }
+
+  getCurrentVoterAccount = () => {
+    return this.state.accounts[this.state.selectedAccount];
+  }
+
   render() {
     return (
       <div>
-        Vote Casting Voter
+        <BallotSignedInfo voters={this.state.votingContract.voters} voterAccount={this.getCurrentVoterAccount()} />
       </div>
     );
   }
