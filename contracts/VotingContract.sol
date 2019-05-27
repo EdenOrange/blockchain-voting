45365dcde3fe6dd577ac5a840ee23dc058b3c38b
Nicholas Thie <ntnicholasthie@gmail.com>
2019-05-27 19:06:21 +0700

    Added VotingContract candidate and organizer

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -11,15 +11,70 @@ contract VotingContract {
     }
     State public state;
 
+    struct Candidate {
+        uint256 id;
+        string name;
+    }
+    Candidate[] public candidates;
+
+    struct BlindSigKey {
+        uint256 N;
+        uint256 E;
+    }
+
+    struct Organizer {
+        string name;
+        BlindSigKey blindSigKey;
+        bool exists;
+    }
+    mapping(address => Organizer) organizers;
+    address[] public organizerAddresses;
+
     // Events
 
     // Functions
-    constructor() public {
+    constructor(string memory name, uint256 N, uint256 E) public {
         state = State.Preparation;
+        organizers[msg.sender] = Organizer(
+            name,
+            BlindSigKey(N, E),
+            true
+        );
+        organizerAddresses.push(msg.sender);
+    }
+
+    modifier onlyOrganizer {
+        require(organizers[msg.sender].exists, "Not an organizer");
+        _;
     }
     // fallback function (if exists)
     // external
     // public
+    function addCandidate(
+        string memory name
+    )
+        public
+        onlyOrganizer
+    {
+        candidates.push(Candidate(candidates.length, name));
+    }
+
+    function addOrganizer(
+        address organizerAddress,
+        string memory name,
+        uint256 N,
+        uint256 E
+    )
+        public
+        onlyOrganizer
+    {
+        organizers[organizerAddress] = Organizer(
+            name,
+            BlindSigKey(N, E),
+            true
+        );
+        organizerAddresses.push(organizerAddress);
+    }
     // internal
     // private
 }
