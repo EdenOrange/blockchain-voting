7c5fc8a4c6cafe8c494b62945e3bf7e25a02c303
Nicholas Thie <ntnicholasthie@gmail.com>
2019-05-27 14:34:18 +0700

    Added VotingContract states

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -1,5 +1,25 @@
 pragma solidity ^0.5.8;
 
 contract VotingContract {
-    
+    // Variables
+    enum State {
+        Preparation,
+        Registration,
+        Voting,
+        Tallying,
+        Finished
+    }
+    State public state;
+
+    // Events
+
+    // Functions
+    constructor() public {
+        state = State.Preparation;
+    }
+    // fallback function (if exists)
+    // external
+    // public
+    // internal
+    // private
 }
