8508eb88403215ad679485c1e3312e1988566d08
Nicholas Thie <ntnicholasthie@gmail.com>
2019-05-29 19:59:45 +0700

    Added temporary testing page frontend, changed organizer key to 256-bit from 2048-bit

--- client/src/Test.js
+++ client/src/Test.js
@@ -2,8 +2,11 @@
 
 import React, { Component } from "react";
 import * as BlindSignature from 'blind-signatures';
+import { BigInteger } from 'jsbn'; 
+import * as Utils from 'web3-utils';
 
 function TestBlindSig(props) {
+  
   const Bob = {
     key: BlindSignature.keyGeneration({ b: 256 }), // b: key-length
     blinded: null,
@@ -87,7 +90,53 @@ function TestBlindSig(props) {
   } else {
     console.log('Bob: Invalid signature');
   }
+/*
+  const N = "58697532336480146441198642100070341275175223310790866838056318326792138477057";
+  const E = "65537";
+  const D = "37675975330979047563902254887797106612462090157231312369036798725649986371681";
+  const message = "0001100111100101000000010011110111101010111010001010101010111001";
+  const blinded = "57221009685133276489649705549076310014986508973798112634818796638756103361497";
+  const r = "27240930816601627203297284412645621648385440502424254114327376784127253912143";
+  const signed = "47320511526484480672545493132411311208752604313027294693269009320216331277974";
+  const unblind = "54002179463205042116184388761924275590714680341223191814795145717257416398460";
+
+  const key = {
+    keyPair: {
+      n: new BigInteger(N),
+      e: new BigInteger(E),
+      d: new BigInteger(D)
+    }
+  }
+
+  const trueSigned = BlindSignature.sign({
+    blinded: blinded,
+    key: key
+  });
+  console.log(signed.toString());
+  console.log(trueSigned.toString());
+  console.log(signed.toString() === trueSigned.toString());
+
+  const trueUnblind = BlindSignature.unblind({
+    signed: signed,
+    N: N,
+    r: r
+  });
+  console.log(unblind.toString());
+  console.log(trueUnblind.toString());
+  console.log(unblind.toString() === trueUnblind.toString());
   
+  const result = BlindSignature.verify({
+    unblinded: unblind,
+    N: N,
+    E: E,
+    message: Utils.soliditySha3(message),
+  });
+  if (result) {
+    console.log('Signatures verify!');
+  } else {
+    console.log('Invalid signature');
+  }
+*/
   return (
     <div></div>
   );
